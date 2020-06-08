5fa2e61100751dc8b9de8cffc8a0fdfeaa198676
Urara <Urara.Nakauchi@gmail.com>
2020-06-08 15:49:58 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+var Chart = require("chart.js");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <canvas id="myChart" width="400" height="400"></canvas>
+
         <a
           className="App-link"
           href="https://reactjs.org"
